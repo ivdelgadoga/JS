8bc38bf47597637314e71464f87057ce033d7c10
ivdelgadoga <ivdelgadoga@izzi.mx>
2026-08-15 13:04:42 -0600

    Comenzando con Some

--- Array_Methods/01_some.js
+++ Array_Methods/01_some.js
@@ -1,1 +1,13 @@
 console.log('Desde 01_some.js');
+
+const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octube', 'Noviembre', 'Diciembre'];
+
+// Recorre el arreglo 
+meses.forEach((mes) => {
+    console.log('Mes: ', mes);
+});
+
+// Crea un nuevo arreglo y lo recorre 
+const nuevoArregloMeses = meses.map((mes) => {});
+
+console.log(nuevoArregloMeses);
