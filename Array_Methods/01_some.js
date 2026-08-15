c1b9d085ea4ce416af6b6ee988b8b161f1202616
ivdelgadoga <ivdelgadoga@izzi.mx>
2026-08-15 13:21:35 -0600

    Finalizamos Some

--- Array_Methods/01_some.js
+++ Array_Methods/01_some.js
@@ -7,7 +7,27 @@ meses.forEach((mes) => {
     console.log('Mes: ', mes);
 });
 
-// Crea un nuevo arreglo y lo recorre 
-const nuevoArregloMeses = meses.map((mes) => {});
+const carrito = [
+    { nombre: 'Alexa Echo Show', cantidad: '9' },
+    { nombre: 'Alexa Echo Spot', cantidad: '2' },
+    { nombre: 'Audifonos Redmi', cantidad: '11' },
+    { nombre: 'Audifonos Gamer', cantidad: '3' },
+    { nombre: 'Barra de Sonido', cantidad: '9' },
+    { nombre: 'Barra de luz LED', cantidad: '50' }
+];
 
-console.log(nuevoArregloMeses);
+const resultado = meses.includes('Marzo');
+
+console.log(resultado); // true, false
+
+// Para arreglos de objetos se utiliza Some 
+
+const existe = carrito.some((item) => {
+    return item.nombre === 'Alexa echo spo'
+});
+
+console.log(existe);
+
+const existeMes = meses.some( mes => mes === 'Enero');
+
+console.log(existeMes);
